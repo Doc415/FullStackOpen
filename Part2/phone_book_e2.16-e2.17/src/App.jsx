f6fb364d0851c8f6eefc13c9801e5f9824e7d221
SERDAR SİPAHİOĞLU <94632226+Doc415@users.noreply.github.com>
2024-08-07 10:40:46 +0300

    exercies 2.16-2.17 completed

---
+++ Part2/phone_book_e2.16-e2.17/src/App.jsx
@@ -0,0 +1,189 @@
+/* eslint-disable react/prop-types */
+import { useState,useEffect } from 'react'
+import personService from './services/persons'
+
+const Notification = ({setMessage,isSuccess}) => {
+  const notificationStyleSuccess= {
+    color:'green',
+    fontStyle: 'italic',
+    fontSize: 24
+  }
+
+  const notificationStyleError= {
+    color:'red',
+    fontStyle: 'strong',
+    fontSize: 24
+  }
+
+
+  console.log(setMessage)
+      if (setMessage==null){
+      return null
+      }
+      else{
+        return(
+          <div style={isSuccess?notificationStyleSuccess:notificationStyleError}>
+            {setMessage}
+          </div>
+        )
+      }
+}
+
+const PersonForm =(props) => {
+  return(
+    <div>
+    <form onSubmit={props.onSubmit}>
+        <div>
+          name: <input value={props.name} onChange={props.onNameChange}/>          
+        </div>
+        <div>number: <input value={props.number} onChange={props.onNumberChange} /></div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+    </div>
+  )
+}
+
+const Persons = ({list,onDelete}) => {
+
+  return(
+    <div>
+      {list.map( person=>
+      <div key={person.id}> <span>{person.name}</span> <span>{person.number}</span>
+      <button  type='button' onClick={()=>onDelete(person.id)}>Remove</button>
+      </div>
+    )}
+    </div>
+ 
+  )
+}
+
+const SearchBox = (props) => {
+  return(
+      <div>
+        search: <input value={props.input} onChange={props.onChange}/>
+      </div>
+  )
+}
+
+const App = () => {
+  const [persons, setPersons] = useState([])
+  const [newName, setNewName] = useState('')
+  const [newPhoneNumber,setNewPhoneNumber]=useState('')
+  const [searchInput, setSearchInput]=useState('')
+  const personList= searchInput===''? persons: persons.filter(person=> person.name.toLowerCase().startsWith(searchInput.toLowerCase()))
+  const [notification,setNotification]=useState(null)
+  const [isSuccess, setIsSuccess]=useState(true)
+
+  useEffect(()=>{personService.getAll().then(initialPersons=> {
+                                       setPersons(initialPersons)}
+                                    )}     
+            ,[])
+
+  const updateSearchResult= (event)=>{
+    setSearchInput(event.target.value)
+
+  }
+
+  const notificationChange=(message)=>{
+    console.log('in notification change')
+    setNotification(message)
+    setTimeout(()=>setNotification(null),3000)
+  }
+  
+  const submitNewName= (event) => {
+      event.preventDefault()
+      const newPerson={ 
+        id:  `${persons.length+1}`,
+        name: newName,
+        number: newPhoneNumber        
+      }
+
+      if (persons.some(person=> person.name===newPerson.name)){
+        if(window.confirm(`${newName} is already recorded! Would you like to update number?`)){
+            const persona= persons.find(person=>person.name===newPerson.name)
+            newPerson.id=persona.id; 
+            personService.updatePerson(newPerson).then(response=> {
+              const updatedPerson=response.data
+              console.log(updatedPerson)
+              const updatedPersons=persons.map(person=> person.id!==updatedPerson.id? person: updatedPerson )
+              setPersons(updatedPersons)            
+              setNewName('')
+              setNewPhoneNumber('') 
+              setIsSuccess(true)
+              notificationChange('Updated successfuly')
+            })
+            return
+        }
+        else
+         {
+          setNewName('')
+          setNewPhoneNumber('')
+          }
+          return
+        }
+
+      console.log(newPerson.id)
+      personService.createPerson(newPerson)
+           .then(response=> {
+              console.log(response.data)            
+              setPersons(persons.concat(response.data))
+              setIsSuccess(true)
+              notificationChange(`Created successfuly`)
+            })
+            .catch(error=> {
+              setIsSuccess(false)
+              notificationChange(`error on creation: ${error}`)}
+            
+            )
+            setNewName('')
+            setNewPhoneNumber('')
+            
+           
+               
+  }
+
+  const updateNewPhoneNumber=(event)=>{
+    setNewPhoneNumber(event.target.value)
+  }
+
+  const updateNewName=(event) => {
+    setNewName(event.target.value)
+  }
+
+  const onDeletePerson= (id) => {
+    if(window.confirm(`This will delete person with id: ${id} Are you sure?`)){
+    console.log(id)
+    personService.deletePerson(id).then(response=>{
+                                            setPersons(persons.filter(person=> person.id!==id))
+                                            setIsSuccess(true)
+                                            notificationChange(`User deleted successfuly`) 
+                                            console.log(response.status)
+                                    })
+                                   .catch(error=> {
+                                    setIsSuccess(false)
+                                    notificationChange(`error on delete: ${error}`)                                    
+                                  })       
+    }                            
+  }
+
+  return (
+    
+    <div>
+      <h2>Phonebook</h2>
+      <hr/>
+      <Notification setMessage={notification} isSuccess={isSuccess}/>
+
+      <h3>Search</h3>
+      <SearchBox input={searchInput} onChange={updateSearchResult}/>
+      <h2>Phonebook</h2>
+      <PersonForm name={newName} number={newPhoneNumber} onNameChange={updateNewName} onNumberChange={updateNewPhoneNumber} onSubmit={submitNewName}/>
+      <h2>Numbers</h2>
+      <Persons list={personList} onDelete={onDeletePerson}/>
+      
+    </div>
+  )
+}
+
+export default App
---
+++ Part2/phone_book_e2.16-e2.17/src/services/persons.js
@@ -0,0 +1,21 @@
+import axios from 'axios'
+const baseUrl='http://localhost:3001/persons/'
+
+const getAll= ()=> {
+    const request=axios.get(baseUrl)
+    return request.then(response=>response.data)
+}
+
+const createPerson= (newPerson) => {
+    return axios.post(baseUrl,newPerson)
+}
+
+const deletePerson=  (id) => {
+    return axios.delete(`${baseUrl}${id}`)
+}
+
+const updatePerson= (person) => {
+    return axios.put(`${baseUrl}${person.id}`,person)    
+}
+
+export default { getAll, createPerson, deletePerson, updatePerson}
